f605d58e9c5919287c5d2a58a711062b290cb661
barophobia <georgerosephone@gmail.com>
2020-09-10 14:23:07 +0100

    practice module 5

---
+++ 5-varPractice/varPractice.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+//Showing the ways to integrate variables in code.
+func main() {
+	var x int = 3
+	var y bool = true
+	var z string = "This is a string"	
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+}
